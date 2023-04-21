b24502e59ad035511f2ab785142fc535b57cce9c
MiguelAMM42 <a93280@alunos.uminho.pt>
2023-04-21 15:21:20 +0100

    2 scripts scorrected

--- Python/activate_debug_mode.sh
+++ Python/activate_debug_mode.sh
@@ -2,7 +2,11 @@
 
 for d in */;
 do 
-cd $d/src/;
-sed -i 's/debug = True/debug = False/g' ${d::-1}.py;
+cd $d;
+cd src/;
+sed -i 's/debug = True/debug = False/g' ${d::-1}_25000.py;
+sed -i 's/debug = True/debug = False/g' ${d::-1}_100000.py;
+sed -i 's/debug = True/debug = False/g' ${d::-1}_250000.py;
+cd ..;
 cd ..;
 done;
