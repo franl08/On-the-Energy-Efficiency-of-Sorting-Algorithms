6878faeb5c9d5689ca880d0efd48b3e6f4916069
franl08 <franeves08@gmail.com>
2023-04-27 11:29:43 +0100

    runs updated

--- bashScripts/runRAPL_large.sh
+++ bashScripts/runRAPL_large.sh
@@ -5,7 +5,7 @@ for d in */; do
     cd "$d"
     echo "cd $d"
     #echo "${d::-1}"
-    if [[ "${d::-1}" == "C" || "${d::-1}" == "CPP" || "${d::-1}" == "CSHARP" || "${d::-1}" == "Go" || "${d::-1}" == "Haskell" || ${d::-1} == "Rust" || "${d::-1}" == "Swift" ]]; then
+    if [[ "${d::-1}" == "C" || "${d::-1}" == "CPP" || "${d::-1}" == "CSHARP" || "${d::-1}" == "Go" || "${d::-1}" == "Haskell" || ${d::-1} == "Rust" || "${d::-1}" == "Swift" || "${d::-1}" == "Python" ]]; then
         if [[ -f "runRAPL_large.sh" ]]; then
             echo "Running RAPL in $d."
             source runRAPL_large.sh
