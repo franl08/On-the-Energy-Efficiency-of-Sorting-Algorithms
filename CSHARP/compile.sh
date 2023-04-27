b43d31ea30a053042d2861b63ee511805c4a4fda
MiguelAMM42 <a93280@alunos.uminho.pt>
2023-04-27 10:46:07 +0100

    Compilation for CSHARP working

--- CSHARP/compile.sh
+++ CSHARP/compile.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-dir_to_process="."
-
-for dir in "$dir_to_process"/*/; do
-  cd "$dir"
-  mcs *.cs
-  cd ".."
+for d in */; do
+  cd "$d"
+  if [ -e "${d::-1}_25000.cs" ]; then
+    mcs "${d::-1}_25000.cs"
+  fi
+  if [ -e "${d::-1}_100000.cs" ]; then
+    mcs "${d::-1}_100000.cs"
+  fi
+  if [ -e "${d::-1}_250000.cs" ]; then
+    mcs "${d::-1}_250000.cs"
+  fi
+  cd ..
 done
