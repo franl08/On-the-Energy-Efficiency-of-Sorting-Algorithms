0a1b73a6ef4b97f3ea4e2ed02310220498d786ac
franl08 <franeves08@gmail.com>
2023-05-04 14:26:11 +0100

    c

--- C/activate_debug_mode.sh
+++ C/activate_debug_mode.sh
@@ -4,7 +4,7 @@ for d in */;
 do 
 cd $d;
 sed -i 's/int debug = 0;/int debug = 1;/g' ${d::-1}_25000.c;
+sed -i 's/int debug = 0;/int debug = 1;/g' ${d::-1}_50000.c;
 sed -i 's/int debug = 0;/int debug = 1;/g' ${d::-1}_100000.c;
-sed -i 's/int debug = 0;/int debug = 1;/g' ${d::-1}_250000.c;
 cd ..;
 done;
