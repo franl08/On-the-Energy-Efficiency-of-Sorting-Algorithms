27920d963226e0c1f90fba2d0faebe23f2d5aa5f
franl08 <franeves08@gmail.com>
2023-05-04 14:34:23 +0100

    c++

--- CPP/activate_debug_mode.sh
+++ CPP/activate_debug_mode.sh
@@ -4,7 +4,7 @@ for d in */;
 do 
 cd $d;
 sed -i 's/bool debug = false;/bool debug = true;/g' ${d::-1}_25000.cpp;
+sed -i 's/bool debug = false;/bool debug = true;/g' ${d::-1}_50000.cpp;
 sed -i 's/bool debug = false;/bool debug = true;/g' ${d::-1}_100000.cpp;
-sed -i 's/bool debug = false;/bool debug = true;/g' ${d::-1}_250000.cpp;
 cd ..;
 done;
