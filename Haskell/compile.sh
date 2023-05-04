4b627fda5da1ca6ad2f1bf583e301cbb22555d61
franl08 <franeves08@gmail.com>
2023-05-04 11:34:54 +0100

    clean haskell

--- Haskell/compile.sh
+++ Haskell/compile.sh
@@ -5,11 +5,11 @@ for d in */; do
   if [ -e "${d::-1}_25000.hs" ]; then
     ghc -O2 --make "${d::-1}_25000.hs" -threaded -rtsopts
   fi
+  if [ -e "${d::-1}_50000.hs" ]; then
+    ghc -O2 --make "${d::-1}_50000.hs" -threaded -rtsopts
+  fi
   if [ -e "${d::-1}_100000.hs" ]; then
     ghc -O2 --make "${d::-1}_100000.hs" -threaded -rtsopts
-  fi
-  if [ -e "${d::-1}_250000.hs" ]; then
-    ghc -O2 --make "${d::-1}_250000.hs" -threaded -rtsopts
   fi
   cd ..
 done
--- Haskell/disable_debug_mode.sh
+++ Haskell/disable_debug_mode.sh
@@ -4,7 +4,7 @@ for d in */;
 do 
 cd $d;
 sed -i 's/main = (print$ '${d::-1}' selectInput nElems)/main = ('${d::-1}' (selectInput nElems)) `seq` (return ())/g' ${d::-1}_25000.hs;
+sed -i 's/main = (print$ '${d::-1}' selectInput nElems)/main = ('${d::-1}' (selectInput nElems)) `seq` (return ())/g' ${d::-1}_50000.hs;
 sed -i 's/main = (print$ '${d::-1}' selectInput nElems)/main = ('${d::-1}' (selectInput nElems)) `seq` (return ())/g' ${d::-1}_100000.hs;
-sed -i 's/main = (print$ '${d::-1}' selectInput nElems)/main = ('${d::-1}' (selectInput nElems)) `seq` (return ())/g' ${d::-1}_250000.hs;
 cd ..;
 done;
